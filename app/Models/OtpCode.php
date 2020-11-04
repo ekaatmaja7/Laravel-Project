1cb402899829caa87705c6503a28a45643847210
ekaatmaja7 <ekaatmaja7@gmail.com>
2020-11-04 08:32:07 +0700

    testing

---
+++ app/Models/OtpCode.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OtpCode extends Model
+{
+    use HasFactory;
+
+    protected static function(){
+        parent::boot();
+
+        static::creating(function ($model){
+            if ( ! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
